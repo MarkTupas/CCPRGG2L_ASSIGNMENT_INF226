4fc732f8ff4742976a4f47c0dc9772ca2fbce1c2
Mark Christian Tupas <119917507+MarkTupas@users.noreply.github.com>
2023-02-10 14:23:48 +0800

    Add files via upload

---
+++ Exercise5/Person.java
@@ -0,0 +1,17 @@
+
+public class Person {
+
+    private String name;
+
+    // get() method
+    public String getName() {
+
+        return this.name;
+    }
+
+    // set() method
+    public void setName(String personName) {
+
+        this.name = personName;
+    }
+}
